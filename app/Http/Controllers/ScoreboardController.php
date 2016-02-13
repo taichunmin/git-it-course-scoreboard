ac92992b69ad665528b1e75e4fcf3f10eb1320a8
taichunmin <taichunmin@gmail.com>
2016-02-13 23:00:54 +0800

    completed/update finish

---
+++ app/Http/Controllers/ScoreboardController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Redis;
+
+class ScoreboardController extends Controller
+{
+    public function completedUpdate(Request $request)
+    {
+        $mid = $request->input('mid'); // machine id
+        $name = $request->input('name') ?: $mid; // user name
+        $completed = $request->input('completed', '[]'); // user completed
+        $completed = json_decode($completed, true) ?: [];
+
+        Redis::pipeline(function ($pipe) use ($mid, $name, $completed) {
+            $pipe->hset('mid:'.$mid, [
+                'mid' => $mid,
+                'name' => $name,
+                'completed' => json_encode($completed),
+            ]);
+            $pipe->sadd('mid_set', $mid);
+        });
+
+        return json_encode(compact('mid', 'name', 'completed'));
+    }
+}
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,6 +15,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::post('completed/update', 'ScoreboardController@completedUpdate');
+
 /*
 |--------------------------------------------------------------------------
 | Application Routes
